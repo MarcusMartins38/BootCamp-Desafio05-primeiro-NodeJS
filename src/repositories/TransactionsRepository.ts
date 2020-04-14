c00ade80a452dee12a22c02617455a1d3143d50b
Marcus Martins <marcusmartins38@gmail.com>
2020-04-14 11:35:55 -0400

    TransactionRepository finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeTotal = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((total, transaction) => total + transaction.value, 0);
+
+    const outcomeTotal = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((total, transaction) => total + transaction.value, 0);
+
+    const total = incomeTotal - outcomeTotal;
+
+    const balance = {
+      income: incomeTotal,
+      outcome: outcomeTotal,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
